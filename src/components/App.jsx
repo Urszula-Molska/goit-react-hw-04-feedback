91dedde2b9a96a8d4a3f1b06f046450cb4421127
Urszula Molska <urszula_molska@wp.pl>
2023-01-14 16:58:07 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.countTotalFeedback = this.countTotalFeedback.bind(this);
+const App = () => {
+  this.countTotalFeedback = this.countTotalFeedback.bind(this);
 
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   handleIncrement = event => {
     const { name } = event.target;
@@ -36,41 +32,39 @@ class App extends Component {
     return percentage;
   };
 
-  render() {
-    const buttonNames = Object.keys(this.state);
+  const buttonNames = Object.keys(this.state);
 
-    return (
-      <div>
-        <Section>
-          <a href="urszula-molska.github.io/goit-react-hw-02-feedback/">
-            urszula-molska.github.io/goit-react-hw-02-feedback
-          </a>
-          <a href="https://github.com/Urszula-Molska/goit-react-hw-02-feedback">
-            https://github.com/Urszula-Molska/goit-react-hw-02-feedback
-          </a>
-        </Section>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={buttonNames}
-            onLeaveFeedback={this.handleIncrement}
+  return (
+    <div>
+      <Section>
+        <a href="urszula-molska.github.io/goit-react-hw-04-feedback/">
+          urszula-molska.github.io/goit-react-hw-04-feedback
+        </a>
+        <a href="https://github.com/Urszula-Molska/goit-react-hw-04-feedback">
+          https://github.com/Urszula-Molska/goit-react-hw-04-feedback
+        </a>
+      </Section>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={buttonNames}
+          onLeaveFeedback={this.handleIncrement}
+        />
+      </Section>
+      {this.countTotalFeedback() === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
           />
         </Section>
-        {this.countTotalFeedback() === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default App;
